034a0c61e088666cfa1f17ca50fdc3669b0df4c3
NirajP911 <nirajp@stella-technologies.com>
2025-09-05 11:44:09 -0400

    Restructure boilerplate code

--- tests/viewReports.spec.js
+++
@@ -1,8 +0,0 @@
-// @ts-check
-import { test, expect } from '@playwright/test';
-
-test.describe ('Tests Reports dashboard', () => {
-    test.skip('Loads reports page', async({page}) => {
-        //Verify Tables are present
-    })
-})
